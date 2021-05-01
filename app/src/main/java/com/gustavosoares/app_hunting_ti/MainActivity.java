255fb03b83e0821932c3ea852e9afc1015433dd1
Gustavo Soares <gustavosoares.adm@gmail.com>
2021-04-30 21:52:46 -0300

    inclusao login e tela principal

--- app/src/main/java/com/gustavosoares/app_hunting_ti/MainActivity.java
+++ app/src/main/java/com/gustavosoares/app_hunting_ti/MainActivity.java
@@ -2,7 +2,12 @@ package com.gustavosoares.app_hunting_ti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btLogin = (Button) findViewById(R.id.bt_login);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText txtLogin = (EditText) findViewById(R.id.txt_login);
+                EditText txtSenha = (EditText) findViewById(R.id.txt_senha);
+                String login = txtLogin.getText().toString();
+                String senha = txtSenha.getText().toString();
+                if (login.equals(senha)) {
+                    Intent intent3 = new Intent(getApplicationContext(), MenuPrincipalActivity.class);
+                    startActivity(intent3);
+                } else{
+                    alert("Nome e Senha devem ser iguais");
+                }
+            }
+        });
     }
+
+    public void irTelaPrincipal(View view){
+
+        Intent intent3 = new Intent(getApplicationContext(), MenuPrincipalActivity.class);
+        startActivity(intent3);
+    }
+
+    private void alert(String s){
+
+        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
+    }
+
 }
---
+++ app/src/main/java/com/gustavosoares/app_hunting_ti/MenuPrincipalActivity.java
@@ -0,0 +1,14 @@
+package com.gustavosoares.app_hunting_ti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+}
