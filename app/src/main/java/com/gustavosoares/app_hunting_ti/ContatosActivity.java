8029e0467de5abcb810d0ca34ee88bb9ed6dfc9f
Gustavo Soares <gustavosoares.adm@gmail.com>
2021-05-29 14:14:13 -0300

    Incluido a funcionalidade de salvar foto na agenda de contatos

--- app/src/main/java/com/gustavosoares/app_hunting_ti/ContatosActivity.java
+++ app/src/main/java/com/gustavosoares/app_hunting_ti/ContatosActivity.java
@@ -1,6 +1,7 @@
 package com.gustavosoares.app_hunting_ti;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -9,7 +10,11 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +23,13 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.GatheringByteChannel;
+import java.util.Calendar;
 
 public class ContatosActivity extends AppCompatActivity {
 
@@ -36,6 +48,8 @@ public class ContatosActivity extends AppCompatActivity {
 
     private final int CAMERA = 1;
     private final int GALERIA =2;
+
+    private final String IMAGE_DIR = "/FotosContatos";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -146,9 +160,41 @@ public class ContatosActivity extends AppCompatActivity {
         startActivityForResult(i, CAMERA);
     }
 
-    private void clicaCarregaImagem(){
-
+    private void clicaCarregaImagem() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            requestGaleriaPermisssion();
+        } else {
+            showGaleria();
+        }
     }
+        private void requestGaleriaPermisssion(){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                Snackbar.make(layout, "É necessário permitir para utilizar a Galeria!",
+                        Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ActivityCompat.requestPermissions(ContatosActivity.this,
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                GALERIA);
+                    }
+                }).show();
+
+            }else{
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        GALERIA);
+            }
+
+        }
+
+        private void showGaleria(){
+            Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(i, GALERIA);
+
+        }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull  String[] permissions, @NonNull  int[] grantResults) {
@@ -157,7 +203,66 @@ public class ContatosActivity extends AppCompatActivity {
                 if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                     clicarTirarFoto();
                 }
-
+                break;
+            case GALERIA:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    clicaCarregaImagem();
+                }
+                break;
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED || data == null) {
+            return;
+        }
+
+        if (requestCode == GALERIA) {
+            Uri contentURI = data.getData();
+
+            try {
+                //salvando imagem nos contatos
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                contato.setFoto(saveImage(bitmap));
+                foto.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == CAMERA) {
+            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            contato.setFoto(saveImage(bitmap));
+            foto.setImageBitmap(bitmap);
+        }
+    }
+
+    //validacao para diminuir a qualidade da imagem salva
+    private String saveImage(Bitmap bitmap){
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50 , bytes);
+        File directory = new File(Environment.getExternalStorageDirectory() + IMAGE_DIR);
+
+        if(!directory.exists()){
+            directory.mkdir();
+        }
+        try {
+            //definindo nome da foto salva (data atual)
+            File f = new File(directory, Calendar.getInstance().getTimeInMillis() + ".jpg");
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+
+            //localizar foto na galeria
+            MediaScannerConnection.scanFile(this, new String[]{f.getPath()}, new String[]{"image/jpeg"}, null);
+            fo.close();
+            return f.getAbsolutePath();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
